e537c1a9adc18a7994660554d524ac7a5e4df368
yangguang <yangguang@bilibili.com>
2017-12-05 17:49:20 +0800

    jinju init

---
+++ src/main/java/com/aguang/jinjuback/JinjuBackApplication.java
@@ -0,0 +1,12 @@
+package com.aguang.jinjuback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinjuBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinjuBackApplication.class, args);
+	}
+}
