056f0f8e94f351bc2a17a4f3423939dec059ca66
li.kai <likai0956@163.com>
2018-05-27 23:26:45 +0800

    tkMybais

---
+++ src/main/java/com/aguang/jinjuback/dao/JinjiuDao.java
@@ -0,0 +1,9 @@
+package com.aguang.jinjuback.dao;
+
+import com.aguang.jinjuback.model.po.Jinju;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface JinjiuDao extends Mapper<Jinju> {
+
+
+}
